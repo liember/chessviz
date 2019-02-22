b65e7fa9c514faa18a082a1545892e561c78506b
liember <liember@yandex.ru>
2019-02-23 03:52:14 +0700

    начал работать над функцией вывода html

--- chessviz.c
+++ chessviz.c
@@ -21,6 +21,42 @@ typedef struct //положение фигРЫ
     int oy;
 } figure_coordinate;
 
+void createhtml(int *table, int *step)
+{
+    printf("<!DOCTYPE HTML\n");
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<meta charset=\"utf-8\">\n");
+    printf("<link rel=\"stylesheet\" href=\"cssforhtml.css\">\n");
+    printf("<title>Chess game</title>\n");
+    printf("</head>\n");
+    printf("<body>\n");
+    printf("<div class=\"contain\">\n");
+    printf("<aside class=\"sidebar\">\n");
+    printf("<p>Chess game</p>\n");
+    printf("</aside>\n");
+    printf("<div class=\"content\">\n");
+    printf("<div class=\"interface\">\n");
+    printf("<div class=\"Chessboard\">\n");
+    //code
+    //code
+    printf("</div>\n");
+    printf("<div class=\"menu\">\n");
+    printf("<div class=\"titl\">\n");
+    printf("<p>Шахматs<p>\n");
+    printf("</div>\n");
+    printf("</div>\n");
+    printf("<div class=\"clear\"></div>\n");
+    printf("</div>\n");
+    printf("</div>\n");
+    printf("</div>\n");
+    printf("</body>\n");
+    printf("</html>\n");
+}
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 int main()
 {
     int chesstable[64] = {BROOK, BKNIGHT, BBISHOP, BQUEEN, BKING, BBISHOP, BKNIGHT, BROOK,
