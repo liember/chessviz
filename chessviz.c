1808c0ad3515e1b0d857317843c8b1047dba24dd
liember <liember@yandex.ru>
2019-02-23 03:07:14 +0700

    инициализация доски

--- chessviz.c
+++ chessviz.c
@@ -11,11 +11,12 @@
 #define WHITE 0
 #define BLACK 1
 
-struct square_coordinate
+typedef struct //положение фигРЫ
 {
+    char figure;
     int ox;
     int oy;
-};
+} figure_coordinate;
 
 int main()
 {
@@ -23,6 +24,42 @@ int main()
     char name[] = "input.inst";
     char a;
     int sum;
+
+    figure_coordinate source_field[32] = {
+        {"R", 1, 1},
+        {"N", 2, 1},
+        {"B", 3, 1},
+        {"Q", 4, 1},
+        {"K", 5, 1},
+        {"B", 6, 1},
+        {"N", 7, 1},
+        {"R", 8, 1},
+        {"P", 1, 2},
+        {"P", 2, 2},
+        {"P", 3, 2},
+        {"P", 4, 2},
+        {"P", 5, 2},
+        {"P", 6, 2},
+        {"P", 7, 2},
+        {"P", 8, 2},
+        {"P", 1, 7},
+        {"P", 2, 7},
+        {"P", 3, 7},
+        {"P", 4, 7},
+        {"P", 5, 7},
+        {"P", 6, 7},
+        {"P", 7, 7},
+        {"P", 8, 7},
+        {"R", 1, 8},
+        {"N", 2, 8},
+        {"B", 3, 8},
+        {"Q", 4, 8},
+        {"K", 5, 8},
+        {"B", 6, 8},
+        {"N", 7, 8},
+        {"R", 8, 8}
+
+    };
 
     if ((fp = fopen(name, "r")) == NULL)
     {
