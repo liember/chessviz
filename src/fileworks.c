694ecdb821662c71ce64a1bdeab071ff77d52417
ivan <liember@yandex.ru>
2019-03-24 17:37:22 +0700

    delete uncessary function

--- src/fileworks.c
+++ src/fileworks.c
@@ -1,13 +1,2 @@
 #include "fileworks.h"
 #include <stdio.h>
-
-int openfile(FILE **inputfile)
-{
-    if ((inputfile = fopen("input", "r")) == NULL)
-    {
-        printf("Не удалось открыть файл шахматных нотаций для воспроизведения партии");
-        getchar();
-        return 1;
-    } // открыть файл удалось
-    return 0;
-}
