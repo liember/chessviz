b3024ddac38f8a12b7c7aa3a53108b1ee3303cc6
ivan <liember@yandex.ru>
2019-03-28 18:30:22 +0700

    convtert parsed for logicworks

--- src/fileworks.c
+++ src/fileworks.c
@@ -1,2 +1,47 @@
 #include "fileworks.h"
 #include <stdio.h>
+
+figure_coordinate identify_figure(char *f)
+{
+    figure_coordinate returnity;
+    returnity.figure = getfigure(f);
+    returnity.ox = getfigureox(f);
+    returnity.oy = getfigureoy(f);
+}
+}
+
+int getfigure(char f)
+{
+    switch (f)
+    {
+    case "K":
+        return WKNIGHT;
+        break;
+    case "Q":
+        return WQUEEN;
+        break;
+    case "R":
+        return WROOK;
+        break;
+    case "N":
+        return WKNIGHT;
+        break;
+    case "B":
+        return WBISHOP;
+        break;
+    case "P":
+        return WPAWN;
+        break;
+    default:
+        printf("No option selected");
+        return ERROR;
+    }
+}
+
+int getfigureox(char f)
+{
+}
+
+int getfigureoy(char f{
+
+}
--- src/fileworks.h
+++ src/fileworks.h
@@ -3,11 +3,25 @@
 
 typedef struct //положение фигРЫ
 {
-    char figure;
+    int figure;
     int ox;
     int oy;
 } figure_coordinate;
 
-figure_coordinate identify_figure(char f);
+int chesstable[64] = {WROOK, WKNIGHT, WBISHOP, WKING, WQUEEN, WBISHOP, WKNIGHT, WROOK,
+                      WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN,
+                      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                      EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+                      BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN,
+                      BROOK, BKNIGHT, BBISHOP, BKING, BQUEEN, BBISHOP, BKNIGHT, BROOK};
+
+FILE *inputfile;
+char result_string[20];
+char *fname = "input.txt";
+int step = 1;
+
+figure_coordinate identify_figure(char *f);
 
 #endif
