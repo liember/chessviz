bce7441ae61aa9e83115af74c07f1bb07f07ecf6
ivan <liember@yandex.ru>
2019-03-28 18:31:30 +0700

    incapsulate variables

--- src/main.c
+++ src/main.c
@@ -5,28 +5,12 @@
 
 int main()
 {
-    int chesstable[64] = {WROOK, WKNIGHT, WBISHOP, WKING, WQUEEN, WBISHOP, WKNIGHT, WROOK,
-                          WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN, WPAWN,
-                          EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-                          EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-                          EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-                          EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-                          BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN, BPAWN,
-                          BROOK, BKNIGHT, BBISHOP, BKING, BQUEEN, BBISHOP, BKNIGHT, BROOK};
-
-    FILE *inputfile;
-
-    char result_string[20];
-    char *fname = "input.txt";
-
-    int step = 1;
 
     inputfile = fopen(fname, "r");
-
     if (inputfile == NULL)
     {
         printf("не могу открыть файл '%s'", fname);
-        return 0;
+        return 1;
     }
 
     while (fgets(result_string, sizeof(result_string), inputfile))
@@ -35,8 +19,8 @@ int main()
         {
             if (result_string[j] == " ")
             {
-                figure_coordinate figure1 = identify_figure(result_string[j + 1]);
-                figure_coordinate figure2 = identify_figure(result_string[j + 5]);
+                figure_coordinate figure1 = identify_figure(&result_string[j + 1]);
+                figure_coordinate figure2 = identify_figure(&result_string[j + 5]);
                 //execute_comand(figure1, result_string[j + 4], figure2);
                 createhtml(step++, chesstable);
             }
