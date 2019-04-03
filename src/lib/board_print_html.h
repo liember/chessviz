aa318372cb313eaf64592a248fcc72863b4e2747
ivan <liember@yandex.ru>
2019-04-04 04:49:59 +0700

    beginner end ended function

--- src/lib/board_print_html.h
+++ src/lib/board_print_html.h
@@ -1,7 +1,9 @@
 #ifndef BOARD_PRINT_HTML_H
 #define BOARD_PRINT_HTML_H
 
-int createfigurehtml(int element, FILE* outputhtmlpage);
+void createfigurehtml(int element, FILE* outputhtmlpage);
 void createhtml(int* table, int step);
+void beginhtml(int* table, int step);
+void endhtml(int* table, int step);
 
 #endif
