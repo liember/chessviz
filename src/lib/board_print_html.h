7a9c34da84c29a4562ec15398b7cecfaface00b8
ivan <liember@yandex.ru>
2019-04-03 16:03:14 +0700

    fix macro

--- src/lib/board_print_html.h
+++ src/lib/board_print_html.h
@@ -1,5 +1,5 @@
-#ifndef DRAWDESK_H
-#define DRAWDESK_H
+#ifndef BOARD_PRINT_HTML_H
+#define BOARD_PRINT_HTML_H
 
 int createfigurehtml(int element, FILE *outputhtmlpage);
 void createhtml(int *table, int step);
