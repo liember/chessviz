f90b272748a1e46909d46f7e6842298234699902
ivan <liember@yandex.ru>
2019-04-04 04:04:38 +0700

    realize moving of other figures

--- src/lib/logicworks.h
+++ src/lib/logicworks.h
@@ -1,22 +1,43 @@
 #ifndef LOGICKWORKS_H
 #define LOGICKWORKS_H
 
-typedef struct {
-    coatingarea* poiter;
-    int ox;
-    int oy;
-} coatingarea;
-
 void swap(int* a, int* b);
 void movepawn(
-        figure_coordinate figure, figure_coordinate figuretarget, int* table);
+        figure_coordinate figure,
+        figure_coordinate figuretarget,
+        int* table,
+        char comand);
+void movebishop(
+        figure_coordinate figure,
+        figure_coordinate figuretarget,
+        int* table,
+        char comand);
+void moverook(
+        figure_coordinate figure,
+        figure_coordinate figuretarget,
+        int* table,
+        char comand);
+void moveking(
+        figure_coordinate figure,
+        figure_coordinate figuretarget,
+        int* table,
+        char comand);
+void movequeen(
+        figure_coordinate figure,
+        figure_coordinate figuretarget,
+        int* table,
+        char comand);
+void moveknight(
+        figure_coordinate figure,
+        figure_coordinate figuretarget,
+        int* table,
+        char comand);
 void execute_comand(
         figure_coordinate figure,
         char comand,
         figure_coordinate figuretarget,
         int* table);
-void atackfigure(
-        figure_coordinate figure, figure_coordinate figuretarget, int* table);
+void atackfigure(int* a, int* b);
 
 void movefigure(
         figure_coordinate figure, figure_coordinate figuretarget, int* table);
